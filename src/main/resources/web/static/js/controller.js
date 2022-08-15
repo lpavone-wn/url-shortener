a29efb4881223cf47d704cbfcf5cc8364e17f151
Gareth Patterson <gareth.patterson@oracle.com>
2022-08-15 16:23:56 +0100

    Minor UI improvements.

--- src/main/resources/web/static/js/controller.js
+++ src/main/resources/web/static/js/controller.js
@@ -1,11 +1,15 @@
 function submitForm() {
   event.preventDefault();
+
+  var responseDiv = document.getElementById("response").hidden = false;
+
   // Validate url field is set
   let url = document.forms["createShortUrlForm"]["url"].value;
   let expiryCode = document.forms["createShortUrlForm"]["expiryCode"].value;
   let validationMessage = document.getElementById("urlValidationMessage");
   if (url === null || url == "") {
     document.getElementById("urlValidationMessage").innerHTML = "Must specify a URL";
+    responseDiv.hidden = true;
     return false;
   } else {
     document.getElementById("urlValidationMessage").innerHTML = "";
@@ -14,14 +18,15 @@ function submitForm() {
   // Call the server to create the URL resource
   var request = new XMLHttpRequest();
 
-  // Open a new connection, using the GET request on the URL endpoint
+  // Create a POST request to the url resource
   request.open('POST', 'http://localhost:8080/shorty/v1/url', true);
   request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 
   request.onload = function () {
-    // Begin accessing JSON data here
+    // Process the response and display the results
     var responseData = JSON.parse(this.response);
     document.getElementById("results").hidden = false;
+    responseDiv.hidden = false;
     var link = document.getElementById("shortUrlLink")
     link.href = responseData.shortUrl;
     link.innerHTML = responseData.shortUrl;
